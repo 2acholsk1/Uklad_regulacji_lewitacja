eb7b3d5da4ae4de1eb8050961dd0e1564b6a7e3a
2acholsk1 <p.zacholski@wp.pl>
2023-01-18 18:08:06 +0100

    first

---
+++ proj.m
@@ -0,0 +1,62 @@
+%%
+clc;
+clear all;
+close all;
+
+%% parametry obiektu i stale
+nr_albumu = [1 4 7 5 8 9];
+
+m = 0.023/(1+nr_albumu(5)+nr_albumu(6)); % kg
+g = 9.81; % m/s^2
+FemP1 = 1.7521*10^-2; % H
+FemP2 = 5.8231*10^-3; % m
+f1 = 1.4142*10^-4; % m*s
+f2 = 4.5626*10^-3; % m
+ki = 2.5165; % A
+ci = 0.0243*sign(nr_albumu(2)-nr_albumu(6)); % A
+d = 0.066*(1 + 0.1*nr_albumu(6)); % m
+bd = 0.06; % m
+xd = d-bd; % m
+
+%% punkt pracy
+x10 = 0.008; % m
+x20 = 0; % m/s
+x30 = 0.2; % A % <--- TU W POLECENIU JEST 0.75, dla 0.2 JEST OK
+u10 = 1/ki*(x30-ci); % [-]
+
+%% obl a_mn
+a21 = 1/2/m * x30^2 * FemP1/FemP2^2 * exp(-x10/FemP2);
+a23 = -1/m * x30 * FemP1/FemP2 * exp(-x10/FemP2);
+a31 = 1/f1 * exp(x10/f2) * (ki*u10 + ci - x30);
+a33 = -f2/f1 * exp(x10/f2);
+b31 = ki * f2/f1 * exp(x10/f2);
+
+%% ctrb/obsv/stab
+A = [0 1 0; a21 0 a23; a31 0 a33];
+B = [0 0; 0 g; b31 0];
+C = [1 0 0];
+D = 0;
+sys = ss(A,B,C,D);
+
+disp("sterowalnosc; obserwowalnosc; stabilnosc");
+rank(ctrb(sys)) == rank(A)
+rank(obsv(sys)) == rank(A)
+isstable(sys)
+
+%% wyznaczanie transmitancji
+G = tf(sys);
+G = G(1);
+Gf = 1/G;
+
+%% nastawy PID
+kp = 80;
+ki = 1/5;
+kd = 1;
+
+%% sprzezenie od stanu
+K = place(A, B(:, 1), [-28, -60, -90]);
+
+
+%% obserwator stanu
+p = [-350, -70, -300];
+L = acker(A', C', p)';
